26fa3a609934797a606e25abd5d6a867fbcfcadb
Devesh Chandra <deveshru2712@gmail.com>
2025-03-23 11:17:23 +0530

    added a protect route

--- backend/src/middleware/protectRoute.ts
+++ backend/src/middleware/protectRoute.ts
@@ -1,3 +1,44 @@
-const protectRoute = () => {};
+import { RequestHandler } from "express";
+import createHttpError from "http-errors";
+import jwt from "jsonwebtoken";
+import env from "../utils/validateEnv";
+import prismaClient from "../config/getClient";
+
+interface JwtPayload {
+  userId: number;
+}
+
+const protectRoute: RequestHandler = async (req, res, next) => {
+  try {
+    const token = req.cookies.key;
+    if (!token) {
+      throw createHttpError(409, "Unauthorized - no token exists");
+    }
+
+    const decode = jwt.verify(token, env.JWT_KEY) as JwtPayload;
+    if (!decode) {
+      throw createHttpError(409, "Unauthorized - invalid token");
+    }
+
+    const user = await prismaClient.user.findUnique({
+      where: { id: decode.userId },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+      },
+    });
+
+    if (!user) {
+      throw createHttpError(404, "User not found!");
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
 export default protectRoute;
